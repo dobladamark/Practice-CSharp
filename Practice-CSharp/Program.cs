ed599fcc1824dc7b45f5caf61a9efcad81c4d7ad
Mark <marknino175@gmail.com>
2026-02-16 07:22:24 +0800

    triangle

--- Practice-CSharp/Program.cs
+++ Practice-CSharp/Program.cs
@@ -125,8 +125,25 @@ namespace Practice_CSharp
 
 
             Console.WriteLine(factorial);
-            */
+            
+            //triangle pattern
+            int num = 0;
 
+            Console.WriteLine("Enter num");
+            num = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < num; i++) 
+            { 
+            
+            for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            
+            }
+            */
         }
     }
 }
